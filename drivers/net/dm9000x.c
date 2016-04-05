9fbefda3fe0e6f986d380a6663b2b56cadf55f32
shift <py.chen1992@gmail.com>
2016-04-05 21:39:48 +0800

    2014-4-5:Porting U-boot, finish the DM9000 by the book, and it works.

--- drivers/net/dm9000x.c
+++ drivers/net/dm9000x.c
@@ -360,13 +360,15 @@ static int dm9000_init(struct eth_device *dev, bd_t *bd)
 	/* Enable TX/RX interrupt mask */
 	DM9000_iow(DM9000_IMR, IMR_PAR);
 
-	i = 0;
-	while (!(phy_read(1) & 0x20)) {	/* autonegation complete bit */
-		udelay(1000);
-		i++;
-		if (i == 10000) {
-			printf("could not establish link\n");
-			return 0;
+	if (phy_read(1) & 0x8) {
+		i = 0;
+		while (!(phy_read(1) & 0x20)) {
+			udelay (1000);
+			i++;
+			if (i == 10000) {
+				printf ("could not establish link.\n");
+				return 0;
+			}
 		}
 	}
 
@@ -554,6 +556,15 @@ static void dm9000_get_enetaddr(struct eth_device *dev)
 	int i;
 	for (i = 0; i < 3; i++)
 		dm9000_read_srom_word(i, dev->enetaddr + (2 * i));
+#else
+	u8 i;
+	char *s, *e;
+	s= getenv ("ethaddr");
+	for (i = 0; i < 6; i++) {
+		dev->enetaddr[i] = s ? simple_strtoul(s, &e, 16) : 0;
+		if (s)
+			s = (*e) ? e + 1 : e;
+	}
 #endif
 }
 
--- include/configs/DIY2440.h
+++ include/configs/DIY2440.h
@@ -64,8 +64,13 @@
  * Hardware drivers
  */
 #define CONFIG_NET_MULTI
-#define CONFIG_CS8900		/* we have a CS8900 on-board */
-#define CONFIG_CS8900_BASE	0x19000300
+#define CONFIG_NET_RETRY_COUNT	20
+#define CONFIG_DRIVER_DM9000	1
+#define CONFIG_DM9000_BASE	0x20000000
+#define DM9000_IO	CONFIG_DM9000_BASE
+#define DM9000_DATA	(CONFIG_DM9000_BASE + 4)
+#define CONFIG_DM9000_USE_16BIT	1
+#define CONFIG_DM9000_NO_SROM	1
 
 /* input clock of PLL */
 //#define CONFIG_SYS_CLK_FREQ	12000000/* the SMDK2410 has 12MHz input clock */
@@ -86,7 +91,6 @@
 //#define CONFIG_NET_MULTI
 //#define CONFIG_CS8900		/* we have a CS8900 on-board */
 //#define CONFIG_CS8900_BASE	0x19000300
-#define CONFIG_CS8900_BUS16	/* the Linux driver does accesses as shorts */
 
 /*
  * select serial console configuration
@@ -122,14 +126,16 @@
 #define CONFIG_CMD_CACHE
 #define CONFIG_CMD_DATE
 #define CONFIG_CMD_ELF
-
+#define CONFIG_CMD_PING
 
 #define CONFIG_BOOTDELAY	3
 /*#define CONFIG_BOOTARGS	"root=ramfs devfs=mount console=ttySA0,9600" */
 /*#define CONFIG_ETHADDR	08:00:3e:26:0a:5b */
+#define CONFIG_ETHADDR	08:90:00:A0:90:90 
 #define CONFIG_NETMASK          255.255.255.0
-#define CONFIG_IPADDR		10.0.0.110
-#define CONFIG_SERVERIP		10.0.0.1
+#define CONFIG_IPADDR		192.168.1.110
+#define CONFIG_SERVERIP		192.168.1.1
+#define CONFIG_GATEWAYIP	192.168.1.1
 /*#define CONFIG_BOOTFILE	"elinos-lart" */
 /*#define CONFIG_BOOTCOMMAND	"tftp; bootm" */
 
